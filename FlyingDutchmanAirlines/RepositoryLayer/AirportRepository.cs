39c1c473aba97b66e3868489422fdf0d68aa364c
Carlos Alberto Diaz Jimenez <ca.diaz@mavi.mx>
2023-02-22 13:18:37 -0600

    AirportRepository

---
+++ FlyingDutchmanAirlines/RepositoryLayer/AirportRepository.cs
@@ -0,0 +1,30 @@
+﻿using FlyingDutchmanAirlines.DatabaseLayer;
+using FlyingDutchmanAirlines.DatabaseLayer.Models;
+using FlyingDutchmanAirlines.Exceptions;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlyingDutchmanAirlines.RepositoryLayer
+{
+    public class AirportRepository
+    {
+        FlyingDutchmanAirlinesContext _context;
+
+        public AirportRepository(FlyingDutchmanAirlinesContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Airport> GetAirportByID(int airportID)
+        {
+            if (airportID < 0)
+            {
+                Console.WriteLine($"Argument Exception in GetAirportByID! AirportID = { airportID}");
+                throw new ArgumentException();
+            }
+
+            return await _context.Airports.FirstOrDefaultAsync(a => a.AirportId == airportID)
+                ?? throw new AirportNotFoundException();
+
+        }
+    }
+}
--- FlyingDutchmanAirlines_Tests/Stubs/FlyingDutchmanAirlinesContext_Stub.cs
+++ FlyingDutchmanAirlines_Tests/Stubs/FlyingDutchmanAirlinesContext_Stub.cs
@@ -24,9 +24,13 @@ namespace FlyingDutchmanAirlines_Tests.Stubs
         public async override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
         {
             IEnumerable<EntityEntry> pendingChanges = ChangeTracker.Entries().Where(e => e.State == EntityState.Added);
+            
             IEnumerable<Booking> bookings = pendingChanges.Select(e => e.Entity).OfType<Booking>();
-
             if (bookings.Any(b => b.CustomerId != 1))
+                throw new Exception("Database Error!");
+
+            IEnumerable<Airport> airports = pendingChanges.Select(e => e.Entity).OfType<Airport>();
+            if (airports.Any(a => a.AirportId == 10))
                 throw new Exception("Database Error!");
 
             await base.SaveChangesAsync(cancellationToken);
